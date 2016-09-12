2a51cb71ef4ae0f7ff4d8029f3e20ad65d4a3cb2
Zach Schultz <zacharymschultz@gmail.com>
2016-09-12 11:37:38 -0400

    Added ES6, Webpack for dev and prod builds and restructured

---
+++ server.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const path = require('path');
+const httpProxy = require('http-proxy');
+const app = express();
+
+let proxy = httpProxy.createProxyServer({
+  changeOrigin: true
+});
+let isProduction = process.env.NODE_ENV === 'production';
+let port = isProduction ? process.env.PORT : 5000;
+let publicPath = path.resolve(__dirname, 'public');
+
+if (!isProduction) {
+  let bundle = require('./server/bundle.js');
+  bundle();
+
+  app.all('/build/*', (req, res) => {
+    proxy.web(req, res, {
+      target: 'http://localhost:8080'
+    });
+  });
+}
+
+// It is important to catch any errors from the proxy or the
+// server will crash. An example of this is connecting to the
+// server when webpack is bundling
+proxy.on('error', function(e) {
+  console.log('Could not connect to proxy, please try again...');
+});
+
+// Link static assets
+app.use(express.static(publicPath));
+
+app.set('port', (port));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+  console.log(`Server running on port ${port}`);
+})
