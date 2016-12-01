80f13e28b1a70d14aa3b9e3aa36886cb3530809c
Ervis Shqiponja <visishqiponja@yahoo.com>
2016-12-01 03:34:41 -0500

    Get all upcoming HW and announcements for all courses

--- src/fbbot/events.js
+++ src/fbbot/events.js
@@ -181,6 +181,49 @@ function getCourseAnnouncements(userId, controller, courseID) {
   });
 }
 
+// Get an array of all the courses currently enrolled
+// Return the array of courses enroled, where each course is simplified.
+function getCoursesEnrolled(userId, controller) {
+  return new Promise((resolve, reject) => {
+    controller.storage.students.getByFBSenderID(userId, (err, user) => {
+      if (err) {
+        console.log('Error finding user');
+        console.log(err);
+        reject('I\'m sorry there was an error.');
+      } else if (!user) {
+        console.log('We couldn\'t find a user for this account, please link your account');
+        reject('We couldn\'t find a user for this account, please link your account');
+      } else if (user.canvas.token) {
+        // Get the user's currently enrolled courses on Canvas
+        sendUtils.getUserCanvasCourses(user.canvas.token)
+        .then((courses) => {
+          const simplifiedCourses = [];
+          courses.forEach((tempCourse) => {
+            const simplifiedCourse = {
+              course_id: tempCourse.id,
+              course_name: tempCourse.name,
+              course_code: tempCourse.course_code,
+            };
+            simplifiedCourses.push(simplifiedCourse);
+            // console.log(simplifiedCourse);
+          });
+
+          // Send it all back. All the grades with minimalistic fields.
+          resolve(simplifiedCourses);
+        })
+        .catch((error) => {
+          console.log('Error getting courses');
+          console.log(error);
+          reject(error);
+        });
+      } else {
+        console.log('We couldn\'t find a token for this account, please link your account');
+        reject('We couldn\'t find a token for this account, please link your account');
+      }
+    });
+  });
+}
+
 function getSchedule(userId, controller) {
   return new Promise((resolve, reject) => {
     controller.storage.students.getByFBSenderID(userId, (err, user) => {
@@ -336,6 +379,109 @@ function alterUserCourseSubscriptions(userId, course, controller, subscribe) {
   });
 }
 
+/*
+* Display all the upcoming assignments given a course ID.
+* First query te upcoming assignments and then display as a list of generics
+*/
+function displayCourseUpcomingHw(bot, message, controller, data) {
+  getCourseAssignments(message.user, controller, data.course_id, bot, message)
+  .then((assignmentsMsg) => {
+    if (assignmentsMsg.length > 0) {
+      bot.reply(message, 'Here are your upcoming assignments from ' + data.course_name);
+
+      assignmentsMsg.forEach((tempAssigmentMsg) => {
+        //  console.log(tempAssigmentMsg.name);
+
+        const dueDateFormatted = moment(tempAssigmentMsg.due_at);
+        const dateNow = moment();
+
+        if (dueDateFormatted.isAfter(dateNow)) {
+          const attachment = {
+            type: 'template',
+            payload: {
+              template_type: 'generic',
+              elements: [
+                {
+                  title: tempAssigmentMsg.name,
+                  subtitle: 'Due Date: ' + dueDateFormatted.format('MMMM Do YYYY, h:mm:ss a') + ', Points: ' + tempAssigmentMsg.points_possible,
+                  // item_url: tempAssigmentMsg.html_url,
+                  default_action: {
+                    type: 'web_url',
+                    url: tempAssigmentMsg.html_url,
+                  },
+                  //image_url: `${SERVER_URL}assets/upcoming_hm.png`,
+                },
+              ],
+            },
+          };
+          bot.reply(message, { attachment });
+        }
+      });
+    } else {
+      bot.reply(message, 'There are no assignments posted at this time. Check back later.');
+    }
+  })
+  .catch((e) => {
+    console.log('Error');
+    console.log(e);
+    bot.reply(message, e);
+  });
+}
+
+/*
+* Display all the announcments given a course ID.
+* First query te announcments and then display as a list of generics
+*/
+function displayCourseAnnouncements (bot, message, controller, data) {
+  getCourseAnnouncements(message.user, controller, data.course_id, bot, message)
+  .then((announcementsMsg) => {
+    const attachment = {
+      type: 'template',
+      payload: {
+        template_type: 'list',
+        top_element_style: 'compact',
+        elements: [
+
+        ],
+        buttons: [
+          {
+            title: 'More Announcements',
+            type: 'web_url',
+            url: CANVAS_URL + 'courses/' + data.course_id + '/announcements',
+          },
+        ],
+      },
+    };
+    for (let i = 0; i < (announcementsMsg.length && 4); i += 1) {
+      const tempAnnouncementMsg = announcementsMsg[i];
+      const postedDate = moment(tempAnnouncementMsg.posted_at).format('MMMM Do YYYY, h:mm:ss a');
+      const newAnnouncementElement = {
+        title: tempAnnouncementMsg.title,
+        subtitle: 'Posted at: ' + postedDate,
+        default_action: {
+          type: 'web_url',
+          url: tempAnnouncementMsg.html_url,
+        },
+        buttons: [
+          {
+            title: 'Read More',
+            type: 'web_url',
+            url: tempAnnouncementMsg.html_url,
+          },
+        ],
+      };
+      attachment.payload.elements.push(newAnnouncementElement);
+    }
+    bot.reply(message, 'Here are the announcements from ' + data.course_name);
+    bot.reply(message, { attachment });
+  })
+  .catch((e) => {
+    console.log('Error');
+    console.log(e);
+    bot.reply(message, e);
+  });
+}
+
 // Take in the Botkit controller and attach events to it
 module.exports = (controller) => {
   // This is triggered when a user clicks the send-to-messenger plugin
@@ -386,51 +532,15 @@ module.exports = (controller) => {
       case 'getUpcomingHw':
         console.log('Assignments Postback!');
         if (!data) {
-          bot.reply(message, 'Here are your upcomming assignments from all your classes.');
-        } else {
-          bot.reply(message, 'Here are your upcomming assignments from ' + data.course_name);
-
-          getCourseAssignments(message.user, controller, data.course_id, bot, message)
-          .then((assignmentsMsg) => {
-            if (assignmentsMsg.length > 0){
-              assignmentsMsg.forEach((tempAssigmentMsg) => {
-                //  console.log("*************************");
-                //  console.log(tempAssigmentMsg.name);
-
-                const dueDateFormatted = moment(tempAssigmentMsg.due_at);
-                const dateNow = moment();
-
-                if (dueDateFormatted.isAfter(dateNow)) {
-                  const attachment = {
-                    type: 'template',
-                    payload: {
-                      template_type: 'generic',
-                      elements: [
-                        {
-                          title: tempAssigmentMsg.name,
-                          subtitle: 'Due Date: ' + dueDateFormatted.format('MMMM Do YYYY, h:mm:ss a') + ', Points: ' + tempAssigmentMsg.points_possible,
-                          // item_url: tempAssigmentMsg.html_url,
-                          default_action: {
-                            type: 'web_url',
-                            url: tempAssigmentMsg.html_url,
-                          },
-                          //image_url: `${SERVER_URL}assets/upcomming_hm.png`,
-                        },
-                      ],
-                    },
-                  };
-                  bot.reply(message, { attachment });
-                }
-              });
-            } else {
-              bot.reply(message, 'There are no assignments posted at this time. Check back later.');
-            }
-          })
-          .catch((e) => {
-            console.log('Error');
-            console.log(e);
-            bot.reply(message, e);
+          bot.reply(message, 'Here are your upcoming assignments from all your classes.');
+          getCoursesEnrolled(message.user, controller, bot, message)
+          .then((courses) => {
+            courses.forEach((tempCourse) => {
+              displayCourseUpcomingHw(bot, message, controller, tempCourse);
+            });
           });
+        } else {
+          displayCourseUpcomingHw(bot, message, controller, data);
         }
         break;
 
@@ -438,55 +548,14 @@ module.exports = (controller) => {
         console.log('Class Announcements Postback!');
         if (!data) {
           bot.reply(message, 'Here are the announcements from all your classes.');
-        } else {
-          bot.reply(message, 'Here are the announcements from ' + data.course_name);
-
-          getCourseAnnouncements(message.user, controller, data.course_id, bot, message)
-          .then((announcementsMsg) => {
-            const attachment = {
-              type: 'template',
-              payload: {
-                template_type: 'list',
-                top_element_style: 'compact',
-                elements: [
-
-                ],
-                buttons: [
-                  {
-                    title: 'More Announcements',
-                    type: 'web_url',
-                    url: CANVAS_URL + 'courses/' + data.course_id + '/announcements',
-                  },
-                ],
-              },
-            };
-            for (let i = 0; i < (announcementsMsg.length && 4); i += 1) {
-              const tempAnnouncementMsg = announcementsMsg[i];
-              const postedDate = moment(tempAnnouncementMsg.posted_at).format('MMMM Do YYYY, h:mm:ss a');
-              const newAnnouncementElement = {
-                title: tempAnnouncementMsg.title,
-                subtitle: 'Posted at: ' + postedDate,
-                default_action: {
-                  type: 'web_url',
-                  url: tempAnnouncementMsg.html_url,
-                },
-                buttons: [
-                  {
-                    title: 'Read More',
-                    type: 'web_url',
-                    url: tempAnnouncementMsg.html_url,
-                  },
-                ],
-              };
-              attachment.payload.elements.push(newAnnouncementElement);
-            }
-            bot.reply(message, { attachment });
-          })
-          .catch((e) => {
-            console.log('Error');
-            console.log(e);
-            bot.reply(message, e);
+          getCoursesEnrolled(message.user, controller, bot, message)
+          .then((courses) => {
+            courses.forEach((tempCourse) => {
+              displayCourseAnnouncements(bot, message, controller, tempCourse);
+            });
           });
+        } else {
+          displayCourseAnnouncements(bot, message, controller, data);
         }
         break;
 
