2246b49f0d3cb6ad9dd6b413327ec57667a69033
Ervis Shqiponja <visishqiponja@yahoo.com>
2016-12-01 04:02:09 -0500

    Get grades for all the courses if needed

--- src/fbbot/events.js
+++ src/fbbot/events.js
@@ -106,7 +106,7 @@ function getCourseAssignments(userId, controller, courseID) {
           });
 
           // Sort them by the due date.
-          simplifiedAssigments.sort( function (a, b) {
+          simplifiedAssigments.sort(function (a, b) {
             return a.due_at < b.due_at;
           });
           // console.log(simplifiedAssigments);
@@ -326,8 +326,7 @@ function getSchedule(userId, controller) {
           console.log(error);
           reject(error);
         });
-      } // else if (user.canvas.token)...
-      else {
+      } else {
         console.log('We couldn\'t find a token for this account, please link your account');
         reject('We couldn\'t find a token for this account, please link your account');
       }
@@ -380,8 +379,54 @@ function alterUserCourseSubscriptions(userId, course, controller, subscribe) {
 }
 
 /*
+* Display all the grades given a course ID.
+* First query the grades and then display as a list of generics
+*/
+function displayCourseGrades(bot, message, controller, data) {
+  bot.reply(message, 'Here are the grades from ' + data.course_name);
+
+  getCourseGrades(message.user, controller, data.course_id, bot, message)
+  .then((gradesMsg) => {
+    if (gradesMsg.grades.length > 0) {
+      gradesMsg.grades.forEach((gradeMsg) => {
+        // console.log("*************************");
+        // console.log(gradeMsg);
+        const gradedDate = moment(gradeMsg.graded_at);
+        const attachment = {
+          type: 'template',
+          payload: {
+            template_type: 'generic',
+            elements: [
+              {
+                title: gradeMsg.assignment_name,
+                subtitle: 'Graded at: ' + gradedDate.format('MMMM Do YYYY, h:mm:ss a') + ', Score: ' + gradeMsg.score + '/' + gradeMsg.assignment_points_possible,
+                // item_url: gradeMsg.html_url,
+                default_action: {
+                  type: 'web_url',
+                  url: gradeMsg.assignment_html_url,
+                },
+              },
+            ],
+          },
+        };
+        bot.reply(message, { attachment });
+      });
+      // Total grade as now.
+      bot.reply(message, 'Total grade: ' + gradesMsg.computed_current_score + '%');
+    } else {
+      bot.reply(message, 'There are no grades at this time. Check back later.');
+    }
+  })
+  .catch((e) => {
+    console.log('Error');
+    console.log(e);
+    bot.reply(message, e);
+  });
+}
+
+/*
 * Display all the upcoming assignments given a course ID.
-* First query te upcoming assignments and then display as a list of generics
+* First query the upcoming assignments and then display as a list of generics
 */
 function displayCourseUpcomingHw(bot, message, controller, data) {
   getCourseAssignments(message.user, controller, data.course_id, bot, message)
@@ -430,50 +475,86 @@ function displayCourseUpcomingHw(bot, message, controller, data) {
 
 /*
 * Display all the announcments given a course ID.
-* First query te announcments and then display as a list of generics
+* First query the announcments and then display as a list of generics
 */
-function displayCourseAnnouncements (bot, message, controller, data) {
+function displayCourseAnnouncements(bot, message, controller, data) {
   getCourseAnnouncements(message.user, controller, data.course_id, bot, message)
   .then((announcementsMsg) => {
-    const attachment = {
-      type: 'template',
-      payload: {
-        template_type: 'list',
-        top_element_style: 'compact',
-        elements: [
+    if (announcementsMsg.length > 0) {
+      bot.reply(message, 'Here are the announcements from ' + data.course_name);
 
-        ],
-        buttons: [
-          {
-            title: 'More Announcements',
-            type: 'web_url',
-            url: CANVAS_URL + 'courses/' + data.course_id + '/announcements',
+      // Facebook allows to send 2 to 4 items in a vertical list. So if ther is
+      // only one announcement we have to send it other way, using a generic template.
+      if (announcementsMsg.length < 2) {
+        const postedDate = moment(announcementsMsg[0].posted_at).format('MMMM Do YYYY, h:mm:ss a');
+        const attachment = {
+          type: 'template',
+          payload: {
+            template_type: 'generic',
+            elements: [
+              {
+                title: announcementsMsg[0].title,
+                subtitle: 'Posted at: ' + postedDate,
+                default_action: {
+                  type: 'web_url',
+                  url: announcementsMsg[0].html_url,
+                },
+                buttons: [
+                  {
+                    title: 'Read More',
+                    type: 'web_url',
+                    url: announcementsMsg[0].html_url,
+                  },
+                ],
+              },
+            ],
           },
-        ],
-      },
-    };
-    for (let i = 0; i < (announcementsMsg.length && 4); i += 1) {
-      const tempAnnouncementMsg = announcementsMsg[i];
-      const postedDate = moment(tempAnnouncementMsg.posted_at).format('MMMM Do YYYY, h:mm:ss a');
-      const newAnnouncementElement = {
-        title: tempAnnouncementMsg.title,
-        subtitle: 'Posted at: ' + postedDate,
-        default_action: {
-          type: 'web_url',
-          url: tempAnnouncementMsg.html_url,
-        },
-        buttons: [
-          {
-            title: 'Read More',
-            type: 'web_url',
-            url: tempAnnouncementMsg.html_url,
+        };
+        bot.reply(message, { attachment });
+      } else {
+        // If more than two announcements, send them as list.
+        const attachment = {
+          type: 'template',
+          payload: {
+            template_type: 'list',
+            top_element_style: 'compact',
+            elements: [
+
+            ],
+            buttons: [
+              {
+                title: 'More Announcements',
+                type: 'web_url',
+                url: CANVAS_URL + 'courses/' + data.course_id + '/announcements',
+              },
+            ],
           },
-        ],
-      };
-      attachment.payload.elements.push(newAnnouncementElement);
+        };
+        for (let i = 0; i < (announcementsMsg.length && 4); i += 1) {
+          const tempAnnouncementMsg = announcementsMsg[i];
+          const postedDate = moment(tempAnnouncementMsg.posted_at).format('MMMM Do YYYY, h:mm:ss a');
+          const newAnnouncementElement = {
+            title: tempAnnouncementMsg.title,
+            subtitle: 'Posted at: ' + postedDate,
+            default_action: {
+              type: 'web_url',
+              url: tempAnnouncementMsg.html_url,
+            },
+            buttons: [
+              {
+                title: 'Read More',
+                type: 'web_url',
+                url: tempAnnouncementMsg.html_url,
+              },
+            ],
+          };
+          attachment.payload.elements.push(newAnnouncementElement);
+        }
+        bot.reply(message, { attachment });
+      }
+    } else {
+      bot.reply(message, 'There are no announcements posted at this time. Check back later.');
     }
-    bot.reply(message, 'Here are the announcements from ' + data.course_name);
-    bot.reply(message, { attachment });
   })
   .catch((e) => {
     console.log('Error');
@@ -563,55 +644,24 @@ module.exports = (controller) => {
         console.log('Class Grades Postback!');
         if (!data) {
           bot.reply(message, 'Here are the grades from all your classes.');
-        } else {
-          bot.reply(message, 'Here are the grades from ' + data.course_name);
-
-          getCourseGrades(message.user, controller, data.course_id, bot, message)
-          .then((gradesMsg) => {
-            if(gradesMsg.grades.length > 0){
-              gradesMsg.grades.forEach((gradeMsg) => {
-                // console.log("*************************");
-                // console.log(gradeMsg);
-                const gradedDate = moment(gradeMsg.graded_at);
-                const attachment = {
-                  type: 'template',
-                  payload: {
-                    template_type: 'generic',
-                    elements: [
-                      {
-                        title: gradeMsg.assignment_name,
-                        subtitle: 'Graded at: ' + gradedDate.format('MMMM Do YYYY, h:mm:ss a') + ', Score: ' + gradeMsg.score + '/' + gradeMsg.assignment_points_possible,
-                        // item_url: gradeMsg.html_url,
-                        default_action: {
-                          type: 'web_url',
-                          url: gradeMsg.assignment_html_url,
-                        },
-                      },
-                    ],
-                  },
-                };
-                bot.reply(message, { attachment });
-              });
-              // Total grade as now.
-              bot.reply(message, 'Total grade: ' + gradesMsg.computed_current_score + '%');
-            } else {
-              bot.reply(message, 'There are no grades at this time. Check back later.');
-            }
-          })
-          .catch((e) => {
-            console.log('Error');
-            console.log(e);
-            bot.reply(message, e);
+          getCoursesEnrolled(message.user, controller, bot, message)
+          .then((courses) => {
+            courses.forEach((tempCourse) => {
+              displayCourseGrades(bot, message, controller, tempCourse);
+            });
           });
+        } else {
+          displayCourseGrades(bot, message, controller, data);
         }
         break;
 
       case 'help':
         console.log('Help Postback!');
         bot.reply(message, 'Here are some of the things you can use me for.');
-        // sendMsg(help)
         break;
+
       default:
+        break;
     }
   });
 
