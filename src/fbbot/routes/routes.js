9444cbe721b1c800fdd3bdcdb77495c4834be65b
Zach Schultz <zacharymschultz@gmail.com>
2016-12-02 16:13:17 -0500

    Refactore fb bot routes to sit on main service so when we are deployed fb can still hit the webhook

--- src/fbbot/routes/routes.js
+++ src/fbbot/routes/routes.js
@@ -2,31 +2,29 @@
 /* eslint-disable camelcase */
 import config from 'config-heroku';
 import express from 'express';
-const router = express.Router();
 
+const router = express.Router();
 const FB_VERIFY_TOKEN = config.fb.verifyToken;
 const facebook_handler = require('../bot').handler;
 
-// module.exports = (app) => {
-  router.get('/', (req, res) => {
-    res.send({ sup: 'sup' });
-  });
+router.get('/', (req, res) => {
+  res.send({ sup: 'sup' });
+});
 
-  router.get('/webhook', (req, res) => {
-    // This enables subscription to the webhooks
-    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
-      res.send(req.query['hub.challenge']);
-    }
-    else {
-      res.send('Incorrect verify token');
-    }
-  });
+router.get('/webhook', (req, res) => {
+  // This enables subscription to the webhooks
+  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
+    res.send(req.query['hub.challenge']);
+  }
+  else {
+    res.send('Incorrect verify token');
+  }
+});
 
-  router.post('/webhook', (req, res) => {
-    facebook_handler(req.body);
+router.post('/webhook', (req, res) => {
+  facebook_handler(req.body);
 
-    res.send('ok');
-  });
-// };
-//
+  res.send('ok');
+});
+
 module.exports = router;
--- src/fbbot/server.js
+++ src/fbbot/server.js
@@ -3,21 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const http = require('http').Server(app);
-const path = require('path');
-
-// app.use(express.static(path.join(__dirname, '/public')))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // view engine ejs
 app.set('view engine', 'ejs')
-
-// Confiure /fbbot routes
-// const router = express.Router();
-// require('./routes/routes')(router);
-//
-// app.use('/fbbot', router);
 
 app.set('port', 3124);
 
